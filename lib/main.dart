fa8dbc9c35f78b4491429bc4e000af631395316b
Sachin Kumar Singh <sa02122000@gmail.com>
2023-12-23 21:49:36 +0530

    Second Commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,8 @@
-
+import 'package:avd_basics/quiz.dart';
 import 'package:flutter/material.dart';
 
-void main(){
-  runApp(MaterialApp(
-    debugShowCheckedModeBanner: false,
-    home: Scaffold(
-      body: Container(color: Colors.deepPurple,
-      child: Center(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Image.asset("assets/images/quiz-logo.png",
-            width: 200,),
-            const SizedBox(height: 30,),
-           const  Text("Learn Flutter the fun way!",
-            style: TextStyle(color: Colors.white,fontSize: 20),),
-            FilledButton(onPressed: (){},
-                child:Text("Start Quz"),),
-          ],
-        ),
-      ),),
-    ),
-  ),);
+void main() {
+  runApp(
+    const Quiz(),
+  );
 }
---
+++ lib/quiz.dart
@@ -0,0 +1,41 @@
+import 'package:avd_basics/questions_screen.dart';
+import 'package:avd_basics/start_screen.dart';
+import 'package:flutter/material.dart';
+
+class Quiz extends StatefulWidget {
+  const Quiz({super.key});
+
+  @override
+  State<Quiz> createState() {
+    return _QuizState();
+  }
+}
+
+class _QuizState extends State<Quiz> {
+  Widget activeScreen = const StartScreen();
+
+  void switchScreen(){
+    setState(() {
+      activeScreen =  const QuestionsScreen();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [Colors.lightBlue, Colors.indigo],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+          child: const StartScreen(),
+        ),
+      ),
+    );
+  }
+}
